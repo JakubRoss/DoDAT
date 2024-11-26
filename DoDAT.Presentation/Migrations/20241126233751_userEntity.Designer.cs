0387084c7fe9237854d1ba7ee1ed30c02f6ace7e
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-11-27 00:38:05 +0100

    Migration user

--- DoDAT.Presentation/Migrations/20241126233751_userEntity.Designer.cs
+++ DoDAT.Presentation/Migrations/20241126233751_userEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DoDAT.Presentation.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoDAT.Presentation.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241126233751_userEntity")]
+    partial class userEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
