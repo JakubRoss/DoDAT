05d7229bb3ea09ad1d8e17ff824f3c7f88aebe0f
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-12-09 22:13:32 +0100

    AddSwaggerGen

--- DoDAT.Presentation/Program.cs
+++ DoDAT.Presentation/Program.cs
@@ -3,6 +3,7 @@ using DoDAT.Application.MIddleware;
 using DoDAT.Infrastructure;
 using DoDAT.Infrastructure.Extension;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.OpenApi.Models;
 
 namespace DoDAT.Presentation
 {
@@ -11,6 +12,47 @@ namespace DoDAT.Presentation
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Dodanie SwaggerGen
+            #region
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "DoDAT API",
+                    Version = "v1",
+                    //Description = "API z uwierzytelnianiem do obslugi ToDo List"
+                    Contact = new OpenApiContact
+                    {
+                        Name = "4q4b",
+                        Email = "jakub.ross@gitify.net",
+                    }
+                });
+
+                options.AddSecurityDefinition("CookieAuth", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Name = "Cookie",
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Cookie,
+                    Description = "Uwierzytelnianie na podstawie ciasteczek"
+                });
+
+                options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "CookieAuth"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+            #endregion
 
             // Dodaj us³ugê CORS
             builder.Services.AddCors(options =>
@@ -48,10 +90,14 @@ namespace DoDAT.Presentation
             // Rejestracja autoryzacji
             builder.Services.AddAuthorization();
 
-            //Rejestracja Serwisow
-
-
             var app = builder.Build();
+
+            app.UseSwagger(); // Dodaje endpoint do specyfikacji JSON (/swagger/v1/swagger.json)
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DoDAT API v1");
+                c.RoutePrefix = string.Empty; // Ustawienie Swaggera na g³ówn¹ œcie¿kê
+            });
 
             // U¿yj CORS
             app.UseCors("AllowVuet");
